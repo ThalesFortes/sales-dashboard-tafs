6a41abf2ec5c0de188222939797e626ee22f7263
ThalesFortes <thales_pro@outlook.com>
2026-02-16 18:07:38 -0300

    Feate: create-profile-low

--- src/hooks/useFormValidation.ts
+++ src/hooks/useFormValidation.ts
@@ -16,8 +16,13 @@ export const useFormValidation = (inputs: InputProps[]) => {
       if (input.type === "email" && value !== "") {
         return /\S+@\S+\.\S+/.test(String(formValues[index]))
       }
-      if (input.type === "password" && value !== "") {
-        return String(formValues[index]).length > 7
+      if (input.type === "password" ) {
+        const password = String(value);
+        const hasCorrectLength = password.length >= 8 && password.length <= 16;
+        const hasUppercaseLetter = /[A-Z]/.test(password);
+        const hasSpecialCharacter = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+        const hasNumber = /\d/.test(password);
+        return hasCorrectLength && hasUppercaseLetter && hasSpecialCharacter && hasNumber
       }
       return true
     })
